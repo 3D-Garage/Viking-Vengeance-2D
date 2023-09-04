56f97f2b763dc148f2e7c1d6ecb09fb5ffb6acbb
balint-szabox <70958880+balint-szabox@users.noreply.github.com>
2023-09-04 21:03:56 +0200

    Running animation activated by code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float runSpeed = 10f;
+    Rigidbody2D myRigidbody2D;
+    Animator myAnimator;
+
     void Start()
     {
-        
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Run();
+    }
+
+    private void Run()
+    {
+        float contolThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(contolThrow * runSpeed, myRigidbody2D.velocity.y);
+        myRigidbody2D.velocity = playerVelocity;
+        FlipSprite();
+        RunningStateChanging();
+    }
+
+    private void RunningStateChanging()
+    {
+        bool runningHorizantaly = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+
+        myAnimator.SetBool("Running", runningHorizantaly);
+
+    }
+
+    private void FlipSprite()
+    {
+        bool runningHorizantaly = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+
+        if (runningHorizantaly)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody2D.velocity.x), 1f);
+        }
     }
 }
