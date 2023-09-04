55a6cfbc0cb3b41be68f0dce9dde1c6801243945
balint-szabox <70958880+balint-szabox@users.noreply.github.com>
2023-09-04 23:53:30 +0200

    Added ability to jump
    - using IsTouchingLayers
    - Droped the friction to zero and prevented wall jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,27 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 15f;
     Rigidbody2D myRigidbody2D;
     Animator myAnimator;
+    //Player Body
+    BoxCollider2D myPlayerBody;
+    //Player Feet
+    PolygonCollider2D myPlayersFeet;
 
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-
+        myPlayerBody = GetComponent<BoxCollider2D>();
+        myPlayersFeet = GetComponent<PolygonCollider2D>();
     }
 
     void Update()
     {
         Run();
+        Jump();
+
     }
 
     private void Run()
@@ -26,6 +34,21 @@ public class Player : MonoBehaviour
         myRigidbody2D.velocity = playerVelocity;
         FlipSprite();
         RunningStateChanging();
+    }
+
+    private void Jump()
+    {
+        //If we arent touching the ground we wont jump in the air
+        if (!myPlayersFeet.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        bool isJumpinmg = CrossPlatformInputManager.GetButtonDown("Jump");
+        if (isJumpinmg)
+        {
+            Vector2 jumpVelocity = new Vector2(myRigidbody2D.velocity.x, jumpSpeed);
+            myRigidbody2D.velocity = jumpVelocity;
+        }
     }
 
     private void RunningStateChanging()
